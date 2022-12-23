b4d603fa377057028c04ac298880424125f1c688
AquilaMS <aquila.botafogo@gmail.com>
2022-12-22 21:34:10 -0300

    new file:   spark/out/best_animations_since_2016.csv/._SUCCESS.crc 	new file:   spark/out/best_animations_since_2016.csv/.part-00000-c595cced-bdd0-4dd9-9b57-b4d312e663aa-c000.csv.crc 	new file:   spark/out/best_animations_since_2016.csv/_SUCCESS 	new file:   spark/out/best_animations_since_2016.csv/part-00000-c595cced-bdd0-4dd9-9b57-b4d312e663aa-c000.csv 	modified:   spark/spark_script.py

--- spark/spark_script.py
+++ spark/spark_script.py
@@ -5,7 +5,7 @@ spark = SparkSession.builder\
     .appName('study_spark')\
     .getOrCreate()
 
-df = spark.read.csv('hdfs:///user/aquila/tests-spark/df_netflix.csv', header=True, inferSchema=True, sep='|')
+df = spark.read.csv('file:////home/aquila/Documents/engdados/hadoop-studies-master/spark/df_netflix.csv', header=True, inferSchema=True, sep='|')
 
 select_col = df.select('title','release_year' ,'imdb_score', 'imdb_votes', 'genres')
 select_imdb = select_col\
@@ -15,4 +15,8 @@ select_imdb = select_col\
     .filter(select_col['imdb_score'] > 7.0)\
     .orderBy(select_col['imdb_votes'], ascending = False)\
     .write.format('csv')\
-    .save('hdfs:///user/aquila/tests-spark/out/best_animations_since_2016.csv')
+    .save('file:////home/aquila/Documents/engdados/hadoop-studies-master/spark/out/best_animations_since_2016.csv')
+
+
+# .save('hdfs:///user/aquila/tests-spark/out/best_animations_since_2016.csv')
+# read.csv('hdfs:///user/aquila/tests-spark/df_netflix.csv'
