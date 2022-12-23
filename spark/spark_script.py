88c2838d996fd9ee6dc9aa590eda3a61bb37b3e4
AquilaMS <aquila.botafogo@gmail.com>
2022-12-22 21:43:42 -0300

    renamed:    spark/out/best_animations_since_2016.csv/._SUCCESS.crc -> spark/out/best_animations_since_2016./._SUCCESS.crc 	renamed:    spark/out/best_animations_since_2016.csv/.part-00000-c595cced-bdd0-4dd9-9b57-b4d312e663aa-c000.csv.crc -> spark/out/best_animations_since_2016./.part-00000-c595cced-bdd0-4dd9-9b57-b4d312e663aa-c000.csv.crc 	renamed:    spark/out/best_animations_since_2016.csv/_SUCCESS -> spark/out/best_animations_since_2016./_SUCCESS 	renamed:    spark/out/best_animations_since_2016.csv/part-00000-c595cced-bdd0-4dd9-9b57-b4d312e663aa-c000.csv -> spark/out/best_animations_since_2016./part-00000-c595cced-bdd0-4dd9-9b57-b4d312e663aa-c000.csv 	modified:   spark/spark_script.py

--- spark/spark_script.py
+++ spark/spark_script.py
@@ -15,7 +15,7 @@ select_imdb = select_col\
     .filter(select_col['imdb_score'] > 7.0)\
     .orderBy(select_col['imdb_votes'], ascending = False)\
     .write.format('csv')\
-    .save('file:////home/aquila/Documents/engdados/hadoop-studies-master/spark/out/best_animations_since_2016')
+    .save('file:////home/aquila/Documents/engdados/hadoop-studies-master/spark/out/best_animations_since_2016.csv')
 
 
 # .save('hdfs:///user/aquila/tests-spark/out/best_animations_since_2016.csv')
