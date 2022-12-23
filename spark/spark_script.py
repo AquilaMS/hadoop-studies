ed12b0e43d37ab504f38262076d1d543a413c384
AquilaMS <aquila.botafogo@gmail.com>
2022-12-22 21:31:16 -0300

    new file:   spark/df_netflix.csv 	new file:   spark/spark_script.py

---
+++ spark/spark_script.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder\
+    .master('local')\
+    .appName('study_spark')\
+    .getOrCreate()
+
+df = spark.read.csv('hdfs:///user/aquila/tests-spark/df_netflix.csv', header=True, inferSchema=True, sep='|')
+
+select_col = df.select('title','release_year' ,'imdb_score', 'imdb_votes', 'genres')
+select_imdb = select_col\
+    .filter(select_col['genres'].contains('animation'))\
+    .filter(select_col['imdb_votes'] > 6000)\
+    .filter(select_col['release_year'] > 2016)\
+    .filter(select_col['imdb_score'] > 7.0)\
+    .orderBy(select_col['imdb_votes'], ascending = False)\
+    .write.format('csv')\
+    .save('hdfs:///user/aquila/tests-spark/out/best_animations_since_2016.csv')
